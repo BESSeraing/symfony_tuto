9aab930899ef4db1ab910de38e84feab66035329
Jona <jona303@gmail.com>
2020-09-29 11:20:36 +0200

    WIP templating integration

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,33 +4,43 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-
-class DefaultController
+// TEmpalte link https://templatemag.com/kompleet-free-multipurpose-bootstrap-template/
+class DefaultController extends AbstractController
 {
 
     /**
      * @Route("/", name="home")
      */
     public function home() {
-        return new Response("<h1>Home</h1>");
+        // On demande à Twig de faire le rendu du template (n'hésitez pas à aller voir le contenu de la methode renderView)
+        $view = $this->renderView('pages/home.html.twig', []);
+        // On met ce rendu dans le corps de la réponse qu'on renvoie
+        return new Response($view);
     }
 
     /**
      * @Route("/search", name="search")
      */
     public function search() {
-        return new Response("<h1>Recherche</h1>");
+        // On demande à Twig de faire le rendu du template (n'hésitez pas à aller voir le contenu de la methode renderView)
+        $view = $this->renderView('pages/search.html.twig', []);
+        // On met ce rendu dans le corps de la réponse qu'on renvoie
+        return new Response($view);
     }
 
     /**
      * @Route("/view/{id<\d+>}", name="showAdvert")
      */
     public function showAdvert(int $id) {
-        return new Response("<h1>Annonce n°".$id."</h1>");
+        // On demande à Twig de faire le rendu du template (n'hésitez pas à aller voir le contenu de la methode renderView)
+        $view = $this->renderView('pages/advert.html.twig', []);
+        // On met ce rendu dans le corps de la réponse qu'on renvoie
+        return new Response($view);
     }
 
     /**
